c84ec9056ab757864269473c50e58aab9aec4d8f
Ayanami951 <156971947+Ayanami951@users.noreply.github.com>
2025-04-28 10:35:29 +0800

    Add files via upload

---
+++ MODBUS-TCP通信小工具/MODBUS-TCP通信小工具/Form1.Designer.cs
@@ -0,0 +1,379 @@
+﻿namespace MODBUS_TCP通信小工具
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_IP = new System.Windows.Forms.TextBox();
+            this.tb_port = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_connect = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tb_addr = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_disconnect = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn_write = new System.Windows.Forms.Button();
+            this.btn_read = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_write = new System.Windows.Forms.TextBox();
+            this.tb_num = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.tb_start = new System.Windows.Forms.TextBox();
+            this.tb_length = new System.Windows.Forms.TextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btn_read2 = new System.Windows.Forms.Button();
+            this.btn_write2 = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tb_IP
+            // 
+            this.tb_IP.Location = new System.Drawing.Point(60, 25);
+            this.tb_IP.Name = "tb_IP";
+            this.tb_IP.Size = new System.Drawing.Size(122, 21);
+            this.tb_IP.TabIndex = 0;
+            // 
+            // tb_port
+            // 
+            this.tb_port.Location = new System.Drawing.Point(60, 62);
+            this.tb_port.Name = "tb_port";
+            this.tb_port.Size = new System.Drawing.Size(122, 21);
+            this.tb_port.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP地址";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "端口号";
+            // 
+            // btn_connect
+            // 
+            this.btn_connect.Location = new System.Drawing.Point(204, 24);
+            this.btn_connect.Name = "btn_connect";
+            this.btn_connect.Size = new System.Drawing.Size(75, 23);
+            this.btn_connect.TabIndex = 3;
+            this.btn_connect.Text = "连接";
+            this.btn_connect.UseVisualStyleBackColor = true;
+            this.btn_connect.Click += new System.EventHandler(this.btn_connect_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(13, 13);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(479, 425);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // tb_addr
+            // 
+            this.tb_addr.Location = new System.Drawing.Point(77, 26);
+            this.tb_addr.Name = "tb_addr";
+            this.tb_addr.Size = new System.Drawing.Size(122, 21);
+            this.tb_addr.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btn_disconnect);
+            this.groupBox1.Controls.Add(this.tb_IP);
+            this.groupBox1.Controls.Add(this.tb_port);
+            this.groupBox1.Controls.Add(this.btn_connect);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(501, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(287, 100);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "TCP连接";
+            // 
+            // btn_disconnect
+            // 
+            this.btn_disconnect.Location = new System.Drawing.Point(204, 62);
+            this.btn_disconnect.Name = "btn_disconnect";
+            this.btn_disconnect.Size = new System.Drawing.Size(75, 23);
+            this.btn_disconnect.TabIndex = 4;
+            this.btn_disconnect.Text = "断开";
+            this.btn_disconnect.UseVisualStyleBackColor = true;
+            this.btn_disconnect.Click += new System.EventHandler(this.btn_disconnect_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.btn_write);
+            this.groupBox2.Controls.Add(this.btn_read);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.tb_write);
+            this.groupBox2.Controls.Add(this.tb_num);
+            this.groupBox2.Controls.Add(this.tb_addr);
+            this.groupBox2.Location = new System.Drawing.Point(501, 119);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(287, 140);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "寄存器读写";
+            // 
+            // btn_write
+            // 
+            this.btn_write.Location = new System.Drawing.Point(204, 102);
+            this.btn_write.Name = "btn_write";
+            this.btn_write.Size = new System.Drawing.Size(75, 23);
+            this.btn_write.TabIndex = 2;
+            this.btn_write.Text = "写入";
+            this.btn_write.UseVisualStyleBackColor = true;
+            this.btn_write.Click += new System.EventHandler(this.btn_write_Click);
+            // 
+            // btn_read
+            // 
+            this.btn_read.Location = new System.Drawing.Point(204, 62);
+            this.btn_read.Name = "btn_read";
+            this.btn_read.Size = new System.Drawing.Size(75, 23);
+            this.btn_read.TabIndex = 2;
+            this.btn_read.Text = "读取";
+            this.btn_read.UseVisualStyleBackColor = true;
+            this.btn_read.Click += new System.EventHandler(this.btn_read_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "写入数据";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "寄存器地址";
+            // 
+            // tb_write
+            // 
+            this.tb_write.Location = new System.Drawing.Point(77, 103);
+            this.tb_write.Name = "tb_write";
+            this.tb_write.Size = new System.Drawing.Size(122, 21);
+            this.tb_write.TabIndex = 0;
+            // 
+            // tb_num
+            // 
+            this.tb_num.Location = new System.Drawing.Point(77, 63);
+            this.tb_num.Name = "tb_num";
+            this.tb_num.Size = new System.Drawing.Size(122, 21);
+            this.tb_num.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 67);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 12);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "读取数量";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.btn_clear);
+            this.groupBox3.Controls.Add(this.btn_write2);
+            this.groupBox3.Controls.Add(this.btn_read2);
+            this.groupBox3.Controls.Add(this.label8);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.richTextBox2);
+            this.groupBox3.Controls.Add(this.tb_length);
+            this.groupBox3.Controls.Add(this.tb_start);
+            this.groupBox3.Location = new System.Drawing.Point(499, 266);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(289, 172);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "线圈读写";
+            // 
+            // tb_start
+            // 
+            this.tb_start.Location = new System.Drawing.Point(79, 20);
+            this.tb_start.Name = "tb_start";
+            this.tb_start.Size = new System.Drawing.Size(122, 21);
+            this.tb_start.TabIndex = 0;
+            // 
+            // tb_length
+            // 
+            this.tb_length.Location = new System.Drawing.Point(79, 58);
+            this.tb_length.Name = "tb_length";
+            this.tb_length.Size = new System.Drawing.Size(122, 21);
+            this.tb_length.TabIndex = 0;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(79, 100);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(122, 66);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(10, 23);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 12);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "起始地址";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(10, 61);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 12);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "报文长度";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(10, 100);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 12);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "写入数据";
+            // 
+            // btn_read2
+            // 
+            this.btn_read2.Location = new System.Drawing.Point(206, 58);
+            this.btn_read2.Name = "btn_read2";
+            this.btn_read2.Size = new System.Drawing.Size(75, 23);
+            this.btn_read2.TabIndex = 3;
+            this.btn_read2.Text = "读取";
+            this.btn_read2.UseVisualStyleBackColor = true;
+            this.btn_read2.Click += new System.EventHandler(this.btn_read2_Click);
+            // 
+            // btn_write2
+            // 
+            this.btn_write2.Location = new System.Drawing.Point(206, 99);
+            this.btn_write2.Name = "btn_write2";
+            this.btn_write2.Size = new System.Drawing.Size(75, 23);
+            this.btn_write2.TabIndex = 3;
+            this.btn_write2.Text = "写入";
+            this.btn_write2.UseVisualStyleBackColor = true;
+            this.btn_write2.Click += new System.EventHandler(this.btn_write2_Click);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(206, 143);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(75, 23);
+            this.btn_clear.TabIndex = 4;
+            this.btn_clear.Text = "清空日志";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_IP;
+        private System.Windows.Forms.TextBox tb_port;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_connect;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox tb_addr;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox tb_write;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_write;
+        private System.Windows.Forms.Button btn_read;
+        private System.Windows.Forms.Button btn_disconnect;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tb_num;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox tb_length;
+        private System.Windows.Forms.TextBox tb_start;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button btn_write2;
+        private System.Windows.Forms.Button btn_read2;
+        private System.Windows.Forms.Button btn_clear;
+    }
+}
+
---
+++ MODBUS-TCP通信小工具/MODBUS-TCP通信小工具/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EasyModbus;
+
+namespace MODBUS_TCP通信小工具
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static ModbusClient mc;
+        public void LogMessage(string message)
+        {
+            richTextBox1.AppendText(DateTime.Now.ToString("yyyy-MM-dd————hh:mm:ss") + "\r\n" + message + "\r\n");
+        }
+
+        //TCP连接
+        private void btn_connect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                mc = new ModbusClient();
+                mc.IPAddress = tb_IP.Text;//设置IP
+                mc.Port = int.Parse(tb_port.Text);//设置端口
+                mc.Connect(mc.IPAddress, mc.Port);//创建连接
+                if(mc.Connected)//判断是否连接成功
+                {
+                    LogMessage("连接成功！");
+                }
+                else
+                {
+                    LogMessage("连接失败！");
+                }
+            }
+            catch(Exception ex) 
+            {
+                LogMessage("连接失败："+ ex.ToString());
+            }
+            
+        }
+
+        //断开连接
+        private void btn_disconnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                mc.Disconnect();
+                if (!mc.Connected)
+                {
+                    LogMessage("连接已断开！");
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        //读寄存器数据
+        private void btn_read_Click(object sender, EventArgs e)
+        {
+            if (mc.Connected)
+            {
+                try
+                {
+                    int addr = int.Parse(tb_addr.Text);
+                    int num = int.Parse(tb_num.Text);
+                    int[] reads = mc.ReadHoldingRegisters(addr, num);
+                    for (int i = 1; i < reads.Length; i++)
+                    {
+                        LogMessage("读取的寄存器数据：" + reads[i].ToString());
+                    }
+                }
+                catch (Exception ex)
+                {
+                    LogMessage("读取失败：" + ex.ToString());
+                }
+            }
+            else
+            {
+                LogMessage("错误！请检测是否连接！");
+            }
+        }
+
+        //写入寄存器数据
+        private void btn_write_Click(object sender, EventArgs e)
+        {
+            if (mc.Connected)
+            {
+                try
+                {
+                    int addr = int.Parse(tb_addr.Text);
+                    int write = int.Parse(tb_write.Text);
+                    mc.WriteSingleRegister(addr, write);
+                    LogMessage("写入成功：" + write.ToString());
+                }
+                catch (Exception ex)
+                {
+                    LogMessage("写入失败：" + ex.ToString());
+                }
+            }
+            else
+            {
+                LogMessage("错误！请检测是否连接！");
+            }
+            
+        }
+
+        //读取线圈数据
+        private void btn_read2_Click(object sender, EventArgs e)
+        {
+            if (mc.Connected)
+            {
+                try
+                {
+                    int start = int.Parse(tb_start.Text);
+                    int length = int.Parse(tb_length.Text);
+                    bool[] reads = mc.ReadCoils(start, length);
+                    for (int i = 0; i < reads.Length; i++)
+                    {
+                        LogMessage("读取的线圈数据：" + reads[i].ToString());
+                    }
+                }
+                catch (Exception ex)
+                {
+                    LogMessage("读取失败：" + ex.ToString());
+                }
+            }
+            else
+            {
+                LogMessage("错误！请检测是否连接！");
+            }
+            
+        }
+
+        //写入线圈数据
+        private void btn_write2_Click(object sender, EventArgs e)
+        {
+            if (mc.Connected)
+            {
+                try
+                {
+                    int start = int.Parse(tb_start.Text);
+                    string writes = richTextBox2.Text;
+                    string[] write_ary = writes.Split(',');
+                    bool[] write_array = new bool[write_ary.Length];
+                    for (int i = 0; i < write_ary.Length; i++)
+                    {
+                        write_array[i] = bool.Parse(write_ary[i]);
+                    }
+                    mc.WriteMultipleCoils(start, write_array);
+                    LogMessage("写入完成！");
+                }
+                catch (Exception ex)
+                {
+                    LogMessage("写入失败：" + ex.ToString());
+                }
+            }
+            else
+            {
+                LogMessage("错误！请检测是否连接！");
+            }
+        }
+
+        //清空日志
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+    }
+}
